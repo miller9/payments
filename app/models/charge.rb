31b41190cf645036f46cbdc1fd4b3d5eb68dfc8a
miller <millerpoli9@gmail.com>
2020-07-11 14:52:22 -0500

    1-backbone. Agrego enums para 'status' y 'payment_method', y Callback para generar 'uid' en modelo Charge.

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -14,4 +14,16 @@
 #
 
 class Charge < ApplicationRecord
+
+  enum status: [:created, :pending, :paid, :rejected, :error]
+  enum payment_method: [:unknown, :credit_card, :debit_card, :pse, :cash, :referenced]
+
+  before_create :generate_uid
+
+  private
+    def generate_uid
+      begin
+        self.uid = SecureRandom.hex(5)
+      end while self.class.exists?(uid: self.uid)
+    end
 end
