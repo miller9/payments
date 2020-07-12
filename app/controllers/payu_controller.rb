219a518119420d22bb154945838e8a25a1275c46
miller <millerpoli9@gmail.com>
2020-07-11 19:27:18 -0500

    2-payU. Segundo y Tercer paso de integracion. Creo controlador 'payu' con funciones 'result', 'confirmation' y sus rutas.

---
+++ app/controllers/payu_controller.rb
@@ -0,0 +1,10 @@
+class PayuController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:confirmation]
+
+  def result
+  end
+
+  def confirmation
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root 'charges#index'
   resources :charges, only: [:new, :create]
+  get "payu/response", to: "payu#result"
+  post "payu/confirmation", to: "payu#confirmation"
 
 end
