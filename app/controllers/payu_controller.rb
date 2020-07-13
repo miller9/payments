a2554ad61fb7f05f686939e5f657c477f9bef98f
miller <millerpoli9@gmail.com>
2020-07-13 16:51:05 -0500

    2-payU. Finalizo el endpoint de PayU.

--- app/controllers/payu_controller.rb
+++ app/controllers/payu_controller.rb
@@ -19,7 +19,12 @@ class PayuController < ApplicationController
       head :unprocessable_entity
       return
     end
+
+    charge.update!(response_data: params.as_json)
+
     if params[:sign] == signature(charge, params[:state_pol])
+      update_status(charge, params[:state_pol])
+      update_payment_method(charge, params[:payment_method_type])
       head :ok
     else
       head :unprocessable_entity
@@ -32,4 +37,33 @@ class PayuController < ApplicationController
       msg = "#{ENV["PAYU_API_KEY"]}~#{ENV["PAYU_MERCHANT_ID"]}~#{charge.uid}~#{charge.amount}~COP~#{state}"
       Digest::MD5.hexdigest(msg)
     end
+
+    def update_status(charge, status)
+      if status == "4"
+        charge.paid!
+      elsif status == "7"
+        charge.pending!
+      elsif status == "6"
+        charge.rejected!
+        charge.update(error_message: params[:response_message_pol])
+      end
+    end
+
+    def update_payment_method(charge, payment_method)
+      if payment_method == "2"
+        charge.credit_card!
+      elsif payment_method == "4"
+        charge.pse!
+      elsif payment_method == "6"
+        charge.debit_card!
+      elsif payment_method == "7"
+        charge.cash!
+      elsif payment_method == "8" || payment_method == "10"
+        charge.referenced!
+      elsif payment_method == "14"
+        charge.transfer!
+      end
+    end
+
+
 end
