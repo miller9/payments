dc4889189155607297e3df5ccc1387340d8fe963
miller <millerpoli9@gmail.com>
2020-07-21 10:15:58 -0500

    3-ePayco-B_Estandar_Checkout. Uso gema httparty en funcion 'result'. Actualizo rutas.

--- app/controllers/epayco_controller.rb
+++ app/controllers/epayco_controller.rb
@@ -2,6 +2,11 @@ class EpaycoController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:confirmation]
 
   def result
+    url =  "https://secure.epayco.co/validation/v1/reference/#{params[:ref_payco]}"
+    response = HTTParty.get(url)
+
+    parsed = JSON.parse(response.body)
+    puts parsed
   end
 
   def confirmation
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'charges#index'
   resources :charges, only: [:new, :create]
 
-  get 'epayco/result'
-  post 'epayco/confirmation'
+  # get 'epayco/result'
+  # post 'epayco/confirmation'
+  get "epayco/response", to: "epayco#result"
+  post "epayco/confirmation", to: "epayco#confirmation"
 
 end
